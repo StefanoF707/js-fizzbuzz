78451467144ca58a9c72e25e5ddb51673c71d74e
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-17 14:20:13 +0100

    programma sulla console

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// FizzBuzz
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// stampare i numeri da 1 a 100
+
+var multi3 = "Fizz";
+
+var multi5 = "Buzz";
+
+var multiBoth = "FizzBuzz";
+
+for ( var i = 1; i <= 100; i++) {
+     
+     if ( (i % 3 == 0) && (i % 5 == 0) ) {
+          console.log(multiBoth);
+     } else if (i % 3 == 0) {
+          console.log(multi3);
+     } else if (i % 5 == 0) {
+          console.log(multi5);
+     } else {
+          console.log(i);
+     }
+
+}
